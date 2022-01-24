decee109cb3fabe13adbc07336dd67b193e48e39
Steven Sosebee <swsosebee@gmail.com>
2022-01-24 18:27:35 -0500

    creating MERN queries in server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,15 +1,82 @@
-const { Tech, Matchup } = require("../models");
 const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
-    me: async (parent, { _id }) => {
-      return User.findById(_id);
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return Users.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
   Mutation: {
-    login: async (parent, { email, password }) => {
-      return Auth;
+    login: async (parent, { username, password }) => {
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this username");
+      }
+
+      const correctPw = await User.checkPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { username, password }) => {
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this username");
+      }
+
+      const correctPw = await User.isPasswordCorrect(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { authors, bookId, image, link }, context) => {
+      try {
+        let updateUser;
+        const userBooks = await Users.findOne({
+          _id: context.user_ID,
+        });
+        console.log(context);
+        // console.log(userRating);
+        const alreadyAdded = userBooks.savedBooks.find(
+          (book) => book.bookId === bookId
+        );
+        console.log(alreadyAdded);
+        if (alreadyAdded) {
+          return userBooks;
+        } else {
+          updateUser = await Users.findOneAndUpdate(
+            { _id: context._ID },
+            { $addToSet: { savedBooks: { authors, bookId, image, link } } },
+            { new: true, runValidators: true }
+          );
+        }
+        return updateUser;
+      } catch (err) {
+        console.log(err);
+      }
+      throw new AuthenticationError("Error in addRating...");
     },
   },
 };
